552f93d66986bed8ae974c50b080a8107f63c03b
skc4365 <50658047+skc4365@users.noreply.github.com>
2025-01-14 14:34:44 +0900

    STS4 연결테스트

---
+++ src/main/java/com/ex/demo/Demo0114Application.java
@@ -0,0 +1,13 @@
+package com.ex.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Demo0114Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Demo0114Application.class, args);
+	}
+
+}
